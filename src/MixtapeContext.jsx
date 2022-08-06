5dffe116cbde5a65340b6aa86e60393c4d0d548c
gnzlplcs <palaciosgm@outlook.com>
2022-08-06 17:45:42 -0500

    add useState values: genre & sortOrder into MixtapeProvider wrapping component

--- src/MixtapeContext.jsx
+++ src/MixtapeContext.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const MixtapeContext = React.createContext();
 
 export const MixtapeProvider = ({ children, songs }) => {
+  const [genre, setGenre] = useState("all");
+  const [sortOrder, setSortOrder] = useState("ascending");
+
   return (
-    <MixtapeContext.Provider value={{ songs }}>
+    <MixtapeContext.Provider value={{ songs, genre, setGenre, sortOrder, setSortOrder }}>
       {children}
     </MixtapeContext.Provider>
   );
