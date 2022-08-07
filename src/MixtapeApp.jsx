089bc30d3feac60581fd8e19702a18650afbf9a9
gnzlplcs <palaciosgm@outlook.com>
2022-08-06 19:30:12 -0500

    render songs

--- src/MixtapeApp.jsx
+++ src/MixtapeApp.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { MixtapeProvider } from "./MixtapeContext";
+import { SongList } from "./SongList";
 
 export const MixtapeApp = () => {
   return (
-    <div className="app">
-      <h1 className="heading">My 🔥 Mixtape</h1>
-      TODO: make some music...? 🎶
-    </div>
+    <MixtapeProvider songs={songs}>
+      <div className="app">
+        <h1 className="heading">My 🔥 Mixtape</h1>
+        TODO: make some music...? 🎶
+        <SongList />
+      </div>
+    </MixtapeProvider>
   );
 };
 
@@ -14,30 +19,30 @@ const songs = [
     artist: "Smash Mouth",
     genre: "pop",
     name: "All Star",
-    year: 1999
+    year: 1999,
   },
   {
     artist: "Drake",
     genre: "rap",
     name: "Hotline Bling",
-    year: 2015
+    year: 2015,
   },
   {
     artist: "Lizzo",
     genre: "hip hop",
     name: "Juice",
-    year: 2019
+    year: 2019,
   },
   {
     artist: "Rick Astley",
     genre: "rock",
     name: "Never Gonna Give You Up",
-    year: 1987
+    year: 1987,
   },
   {
     artist: "4 Non Blondes",
     genre: "rock",
     name: "What's Up",
-    year: 1993
-  }
+    year: 1993,
+  },
 ];
